517f28d4e59f304779899432e0275afdaf6208db
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-17 10:54:27 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
-# Placeholder for config.py
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
