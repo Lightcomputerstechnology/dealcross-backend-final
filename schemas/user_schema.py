201b692418494a43e81a04051a20e9a81ad064e8
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-17 10:23:09 +0100

    Update user_schema.py

--- schemas/user_schema.py
+++ schemas/user_schema.py
@@ -1,14 +1,19 @@
-# schemas/user_schema.py
-
 from pydantic import BaseModel, EmailStr
+from typing import Optional
+
 
 class UserCreate(BaseModel):
+    username: str
     email: EmailStr
     password: str
 
+    model_config = {"from_attributes": True}
+
+
 class UserOut(BaseModel):
     id: int
+    username: str
     email: EmailStr
+    is_active: bool
 
-    class Config:
-        orm_mode = True
+    model_config = {"from_attributes": True}
