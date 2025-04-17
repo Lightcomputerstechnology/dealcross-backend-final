1a392bc77ec251b090878cd38855d36a3c6d90fe
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-17 14:58:25 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,19 +1,8 @@
-from .user import User
-from .deal import Deal
-from .wallet import Wallet
-from .share import Share
-from .dispute import Dispute
-from .escrow_tracker import EscrowTracker
-from .settings import AppSettings
-from .aiinsight import AIInsight
-
-__all__ = [
-    "User",
-    "Deal",
-    "Wallet",
-    "Share",
-    "Dispute",
-    "EscrowTracker",
-    "AppSettings",
-    "AIInsight",
-]
+from .user            import User
+from .deal            import Deal
+from .wallet          import Wallet
+from .share           import Share
+from .dispute         import Dispute
+from .escrow_tracker  import EscrowTracker
+from .settings        import AppSettings
+from .aiinsight       import AIInsight        # ← add this
