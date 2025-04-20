aa980fe823aab045c7fc209250bcebbd346cd6b0
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-20 18:01:35 +0100

    Update metric.py

--- models/metric.py
+++ models/metric.py
@@ -1,4 +1,4 @@
-# File: models/metric.py
+# # File: models/metric.py
 
 from sqlalchemy import Column, Integer, String, Float, DateTime
 from datetime import datetime
@@ -8,6 +8,6 @@ class Metric(Base):
     __tablename__ = "metrics"
 
     id = Column(Integer, primary_key=True, index=True)
-    type = Column(String, index=True)  # e.g., 'users', 'deals'
+    type = Column(String, index=True)  # e.g., 'users', 'deals', 'wallets_funded'
     value = Column(Float)
     timestamp = Column(DateTime, default=datetime.utcnow)
