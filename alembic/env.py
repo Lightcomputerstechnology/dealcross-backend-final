2d2848ea3c4c46012ff7b94f8f9495ce9706ef98
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-22 22:58:58 +0100

    Update env.py

--- alembic/env.py
+++ alembic/env.py
@@ -1,6 +1,9 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from logging.config import fileConfig
