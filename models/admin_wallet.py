4bed8b0e464559247815155c8b057e47549feef8
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-23 17:29:58 +0100

    Create admin_wallet.py

---
+++ models/admin_wallet.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Integer, Numeric
+from core.database import Base
+
+class AdminWallet(Base):
+    __tablename__ = 'admin_wallet'
+
+    id = Column(Integer, primary_key=True, index=True)
+    balance = Column(Numeric(12, 2), default=0.00)
