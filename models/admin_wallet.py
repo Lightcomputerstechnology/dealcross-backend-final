095a779f626d12720decd00269a59846a02e9f6d
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-25 06:29:29 +0100

    Update admin_wallet.py

--- models/admin_wallet.py
+++ models/admin_wallet.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, Numeric
 from core.database import Base
 
 class AdminWallet(Base):
-    __tablename__ = 'admin_wallet'
+    __tablename__ = "admin_wallet"
 
     id = Column(Integer, primary_key=True, index=True)
     balance = Column(Numeric(12, 2), default=0.00)
