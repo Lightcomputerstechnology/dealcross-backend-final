6e2dfddd700dfbf1d91df3620219112dd291fddc
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-27 21:14:18 +0100

    Delete user.py

--- user.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import Table, MetaData
-from core.database import engine
-
-# ✅ Reflect the existing 'users' table from the database
-metadata = MetaData()
-User = Table('users', metadata, autoload_with=engine)
