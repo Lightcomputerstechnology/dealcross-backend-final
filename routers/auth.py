84deaf3df35c6e9b617059541cf7c14010fab785
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-27 18:32:42 +0100

    Update auth.py

--- routers/auth.py
+++ routers/auth.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from core.security import verify_password, get_password_hash, get_current_user, create_access_token
-from models.user import User
+from models import User
 from models.login_attempt import LoginAttempt
 from core.database import get_db
 from schemas.user import UserCreate, UserOut, Token
