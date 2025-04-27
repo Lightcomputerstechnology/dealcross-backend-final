6e970e72819114b511a74b029637ab9954cba28f
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-27 17:13:10 +0100

    Update user_profile.py

--- routers/user_profile.py
+++ routers/user_profile.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from core.database import get_db
 from core.security import get_current_user, get_password_hash
-from models.user import User
+from models import User
 from schemas.user import UserOut, UserUpdate
 
 router = APIRouter(
