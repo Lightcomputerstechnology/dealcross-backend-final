fd2f222f99fe62659a3519eaffb2a6b06d8fe27f
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-27 19:08:24 +0100

    Update secure_admin.py

--- routers/secure_admin.py
+++ routers/secure_admin.py
@@ -3,7 +3,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from core.database import get_db
-from models.user import User
+from models import User
 from core.security import get_current_user
 from typing import List
 
