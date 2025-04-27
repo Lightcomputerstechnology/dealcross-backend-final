7b28e9162cb74f5a3c61b06fe6295140131bc924
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-27 17:18:09 +0100

    Update deals.py

--- routers/deals.py
+++ routers/deals.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Session
 from core.database import get_db
 from core.security import get_current_user
 from models.deal import Deal, DealStatus
-from models.user import User
+from models import User
 from models.wallet import Wallet
 from models.wallet_transaction import WalletTransaction
 from schemas.deal import DealCreate, DealOut
