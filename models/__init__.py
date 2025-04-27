6988bea664a814f8d82f9c9343fab0a92f8e7bc7
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-27 14:49:15 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,14 +1,2 @@
 from .user import User
 from .kyc import KYCRequest
-from .deal import Deal
-from .wallet import Wallet
-from .share import Share
-from .dispute import Dispute
-from .escrow_tracker import EscrowTracker
-from .settings import AppSettings
-from .aiinsight import AIInsight
-from .fee_transaction import FeeTransaction
-from .admin_wallet import AdminWallet
-from .fraud import FraudAlert
-from .audit import AuditLog
-# from .notification import Notification  # Uncomment only if ready
