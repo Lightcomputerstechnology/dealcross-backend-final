8399c52512103f3d86024f92d0ec38bea9636e5c
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-28 18:08:40 +0100

    Update share.py

--- models/share.py
+++ models/share.py
@@ -1,5 +1,5 @@
 class Share(Model):
     id = fields.IntField(pk=True)
-    name = fields.CharField(max_length=255)
-    price = fields.FloatField()
-    change = fields.CharField(max_length=50, null=True)
+    company_name = fields.CharField(max_length=255)
+    price = fields.DecimalField(max_digits=12, decimal_places=2)
+    created_at = fields.DatetimeField(auto_now_add=True)
