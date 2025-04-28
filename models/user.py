03fa53866e2bc82116641e3aa611e02498747f39
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-28 21:18:30 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,9 +1,17 @@
 from tortoise.models import Model
 from tortoise import fields
+import enum
+
+class UserRole(str, enum.Enum):
+    user = "user"
+    moderator = "moderator"
+    auditor = "auditor"
+    admin = "admin"
+
 class User(Model):
     id = fields.IntField(pk=True)
     username = fields.CharField(max_length=255, unique=True)
     email = fields.CharField(max_length=255, unique=True)
     password = fields.CharField(max_length=255)
-    role = fields.CharField(max_length=255, default="user")
+    role = fields.CharEnumField(UserRole, default=UserRole.user)
     created_at = fields.DatetimeField(auto_now_add=True)
