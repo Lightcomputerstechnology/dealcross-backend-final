936673978234b991e4840c51423071e35ef464c9
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-28 18:42:56 +0100

    Update pairing.py

--- models/pairing.py
+++ models/pairing.py
@@ -1,3 +1,5 @@
+from tortoise.models import Model
+from tortoise import fields
 class Pairing(Model):
     id = fields.IntField(pk=True)
     creator = fields.ForeignKeyField("models.User", related_name="pairings", on_delete=fields.CASCADE)
