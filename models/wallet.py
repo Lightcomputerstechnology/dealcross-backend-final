a27de70c71f872b75f665c6ddca3dd8e302121f8
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-28 13:26:17 +0100

    Update wallet.py

--- models/wallet.py
+++ models/wallet.py
@@ -6,3 +6,6 @@ class Wallet(Model):
     user = fields.ForeignKeyField("models.User", related_name="wallet", on_delete=fields.CASCADE)
     balance = fields.DecimalField(max_digits=12, decimal_places=2, default=0.00)
     updated_at = fields.DatetimeField(auto_now=True)
+
+    class Meta:
+        table = "wallet"  # Ensure the table name matches your database structure
