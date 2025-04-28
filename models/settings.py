2fd940428749d0fd6aa7783d74c0dc0bec9c56b7
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-28 18:43:21 +0100

    Update settings.py

--- models/settings.py
+++ models/settings.py
@@ -1,3 +1,5 @@
+from tortoise.models import Model
+from tortoise import fields
 class AppSettings(Model):
     id = fields.IntField(pk=True)
     setting_name = fields.CharField(max_length=255, unique=True)
