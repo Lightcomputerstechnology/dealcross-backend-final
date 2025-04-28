70d600f8ba4d82abe90f078e7e412a367376ae77
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-28 18:34:29 +0100

    Update admin_wallet.py

--- models/admin_wallet.py
+++ models/admin_wallet.py
@@ -1,3 +1,6 @@
+from tortoise.models import Model
+from tortoise import fields
+
 class AdminWallet(Model):
     id = fields.IntField(pk=True)
     balance = fields.DecimalField(max_digits=12, decimal_places=2, default=0.00)
