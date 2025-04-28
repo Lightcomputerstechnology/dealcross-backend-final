9fc8126cc650654e7d4ff8471ca0ab497c05d073
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-28 23:04:49 +0100

    Update wallet.py

--- models/wallet.py
+++ models/wallet.py
@@ -1,7 +1,18 @@
+# File: src/models/wallet.py
+
 from tortoise.models import Model
 from tortoise import fields
+from models.user import User
+
 class Wallet(Model):
     id = fields.IntField(pk=True)
-    user = fields.ForeignKeyField("models.User", related_name="wallet", on_delete=fields.CASCADE)
+    user = fields.ForeignKeyField("models.User", related_name="wallet")
     balance = fields.DecimalField(max_digits=12, decimal_places=2, default=0.00)
+    created_at = fields.DatetimeField(auto_now_add=True)
     updated_at = fields.DatetimeField(auto_now=True)
+
+    class Meta:
+        table = "wallets"
+
+    def __str__(self):
+        return f"{self.user.username} - Balance: {self.balance} USD"
