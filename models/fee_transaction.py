a7788ada97ef2d3895281ea2fc28dbff8f60eacf
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-28 18:39:17 +0100

    Update fee_transaction.py

--- models/fee_transaction.py
+++ models/fee_transaction.py
@@ -1,3 +1,5 @@
+from tortoise.models import Model
+from tortoise import fields
 class FeeTransaction(Model):
     id = fields.IntField(pk=True)
     amount = fields.DecimalField(max_digits=12, decimal_places=2)
