31491e1652de8aa21630b30148d34c25cad4ab0e
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-28 06:51:23 +0100

    Update admin_wallet.py

--- models/admin_wallet.py
+++ models/admin_wallet.py
@@ -4,6 +4,7 @@ from core.database import Base
 
 class AdminWallet(Base):
     __tablename__ = "admin_wallet"
+    __table_args__ = {'extend_existing': True}  # Allow table reuse
 
     id = Column(Integer, primary_key=True, index=True)
     balance = Column(Numeric(12, 2), default=0.00)
