01779f9ce6cf3c7226ba3b8685a8d58089ec2e2f
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-28 18:10:24 +0100

    Update wallet.py

--- models/wallet.py
+++ models/wallet.py
@@ -1,11 +1,5 @@
-from tortoise import fields
-from tortoise.models import Model
-
 class Wallet(Model):
     id = fields.IntField(pk=True)
     user = fields.ForeignKeyField("models.User", related_name="wallet", on_delete=fields.CASCADE)
     balance = fields.DecimalField(max_digits=12, decimal_places=2, default=0.00)
     updated_at = fields.DatetimeField(auto_now=True)
-
-    class Meta:
-        table = "wallet"  # Ensure the table name matches your database structure
