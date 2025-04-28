71db537d93f63807995bc35e7857a16c4470abc6
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-28 18:40:47 +0100

    Update login_attempt.py

--- models/login_attempt.py
+++ models/login_attempt.py
@@ -1,3 +1,5 @@
+from tortoise.models import Model
+from tortoise import fields
 class LoginAttempt(Model):
     id = fields.IntField(pk=True)
     user = fields.ForeignKeyField("models.User", related_name="login_attempts", on_delete=fields.CASCADE)
