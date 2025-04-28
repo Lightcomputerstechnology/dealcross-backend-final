1b350ac70971e81cb043cd6c693e4b7d7a71d095
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-28 18:37:32 +0100

    Update deal.py

--- models/deal.py
+++ models/deal.py
@@ -1,3 +1,5 @@
+from tortoise.models import Model
+from tortoise import fields
 class Deal(Model):
     id = fields.IntField(pk=True)
     user = fields.ForeignKeyField("models.User", related_name="deals", on_delete=fields.CASCADE)
