12f930c2848251f6e72012ed050b69c115602769
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-28 18:42:19 +0100

    Update notification.py

--- models/notification.py
+++ models/notification.py
@@ -1,3 +1,5 @@
+from tortoise.models import Model
+from tortoise import fields
 class Notification(Model):
     id = fields.IntField(pk=True)
     user = fields.ForeignKeyField("models.User", related_name="notifications", on_delete=fields.CASCADE)
