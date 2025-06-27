dbed0b7fa70b69ac1c35e0843a0d8d7d4c61c03d
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-06-27 06:04:30 +0100

    Update otp.py

--- utils/otp.py
+++ utils/otp.py
@@ -1,13 +1,14 @@
-# utils/otp.py
-import pyotp
-from models.user import User
+# File: utils/otp.py
 
-def generate_totp_secret() -> str:
+import pyotp
+
+def generate_totp_secret():
     return pyotp.random_base32()
 
-def get_totp_uri(user: User, secret: str, issuer: str) -> str:
-    return pyotp.totp.TOTP(secret).provisioning_uri(name=user.email, issuer_name=issuer)
+def generate_totp_uri(username: str, secret: str, issuer: str = "Dealcross"):
+    totp = pyotp.TOTP(secret)
+    return totp.provisioning_uri(name=username, issuer_name=issuer)
 
-def verify_totp_code(secret: str, code: str) -> bool:
+def verify_totp_code(secret: str, code: str):
     totp = pyotp.TOTP(secret)
     return totp.verify(code, valid_window=1)
