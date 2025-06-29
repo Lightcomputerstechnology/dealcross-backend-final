8903f4c0c4b157ceb0c36f0ae037d07fa5b721ed
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-06-29 06:21:07 +0100

    Update send_email.py

--- utils/send_email.py
+++ utils/send_email.py
@@ -3,7 +3,7 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from config import settings
+from project_config.dealcross_config import settings
 
 async def send_email(to: str, subject: str, body: str):
     msg = MIMEMultipart()
