4d8ed4ac6f46ceb1f51c2f2f0ed9cb74b6aa600d
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-07-01 01:04:44 +0100

    Update config.py

--- core/config.py
+++ core/config.py
@@ -2,31 +2,14 @@ from pydantic_settings import BaseSettings
 from pydantic import Field
 
 class Settings(BaseSettings):
-    # General
     app_name: str = Field(..., alias="APP_NAME")
     app_env: str = Field(..., alias="APP_ENV")
     app_port: int = Field(..., alias="APP_PORT")
-
-    # Database
     database_url: str = Field(..., alias="DATABASE_URL")
-
-    # Security
     jwt_secret: str = Field(..., alias="JWT_SECRET")
     algorithm: str = Field(..., alias="ALGORITHM")
     access_token_expire_minutes: int = Field(60, alias="ACCESS_TOKEN_EXPIRE_MINUTES")
-
-    # Redis
     redis_url: str = Field(..., alias="REDIS_URL")
-
-    # Email
-    smtp_server: str = Field(..., alias="EMAIL_HOST")
-    smtp_port: int = Field(..., alias="EMAIL_PORT")
-    smtp_username: str = Field(..., alias="EMAIL_USER")
-    smtp_password: str = Field(..., alias="EMAIL_PASSWORD")
-    smtp_from: str = Field(..., alias="EMAIL_FROM_NAME")
-
-    # Frontend
-    frontend_url: str = Field(..., alias="FRONTEND_URL")
 
     model_config = {
         "env_file": ".env",
@@ -35,3 +18,18 @@ class Settings(BaseSettings):
     }
 
 settings = Settings()
+
+TORTOISE_ORM = {
+    "connections": {
+        "default": settings.database_url,
+    },
+    "apps": {
+        "models": {
+            "models": [
+                "models",         # <- will pick up all models with __models__ inside
+                "aerich.models"
+            ],
+            "default_connection": "default",
+        }
+    }
+}
