623c7f4f450e7e7661e3362622ea60ed85640120
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-09-06 02:55:46 +0100

    Update auth.py

--- core/auth.py
+++ core/auth.py
@@ -4,7 +4,7 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import jwt, JWTError
 from pydantic import BaseModel
-from core.settings import settings  # ✅ Correct import
+from project_config.dealcross_config import settings
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
